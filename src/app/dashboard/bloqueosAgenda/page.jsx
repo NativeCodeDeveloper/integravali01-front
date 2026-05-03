234d85052ef95d0527d456011e53c5ddcc91e799
NativeCode <tm.nicolasmachuca@gmail.com>
2026-05-02 23:36:54 -0400

    asdasd

--- src/app/dashboard/bloqueosAgenda/page.jsx
+++ src/app/dashboard/bloqueosAgenda/page.jsx
@@ -458,7 +458,7 @@ const [id_profesional, setId_profesional] = useState("");
                                         <span className="flex items-center justify-center gap-2">
                                             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             </svg>
-                                            Cargar Todos los Bloqueos
+                                            Ver listado de bloqueos
                                         </span>
                                     </button>
 
